528b56cda415d72e83e87b811401729b46b7feb1
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-01 14:13:52 +0000

    Add day 1 part 2 and refactor

--- day1.js
+++ day1.js
@@ -1,16 +1,31 @@
 const fs = require("fs");
 
-function problem1(fileName) {
-  return fs
+const preProcess = (fileName) =>
+  fs
     .readFileSync(fileName)
     .toString()
     .split("\n")
-    .map((item) => parseInt(item, 10))
+    .map((item) => parseInt(item, 10));
+
+function problem1(array) {
+  return array.reduce(
+    (previousValue, currentValue, currentIndex, array) =>
+      (currentIndex === 1 ? 0 : previousValue) +
+      (currentValue > array[currentIndex - 1] ? 1 : 0)
+  );
+}
+
+function problem2(array) {
+  return array
+    .map((curr, index, arr) =>
+      index + 2 < arr.length ? curr + arr[index + 1] + arr[index + 2] : null
+    )
+    .filter(Boolean)
     .reduce(
       (previousValue, currentValue, currentIndex, array) =>
         (currentIndex === 1 ? 0 : previousValue) +
         (currentValue > array[currentIndex - 1] ? 1 : 0)
     );
 }
-
-console.log(problem1("1.1-input.txt"));
+console.log(problem1(preProcess("1.1-input.txt")));
+console.log(problem2(preProcess("1.1-input.txt")));
