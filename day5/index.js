24888b5fecfb39274741e3d647c8332d0d289c48
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-05 12:40:00 +0000

    Day5 Part 2

--- day5/index.js
+++ day5/index.js
@@ -59,4 +59,57 @@ function problem1(array) {
     ).length;
 }
 
-console.log(problem1(fileToArray("day5/input.txt")));
+function rangeRefined(arr) {
+  let [start, end] = arr;
+  if (start > end) {
+    [start, end] = [end, start];
+    return Array(end - start + 1)
+      .fill()
+      .map((_, i) => start + i)
+      .reverse();
+  }
+  return Array(end - start + 1)
+    .fill()
+    .map((_, i) => start + i);
+}
+
+function processDiagonal(entry) {
+  let xRange = rangeRefined([entry[0][0], entry[1][0]]);
+  let yRange = rangeRefined([entry[0][1], entry[1][1]]);
+  xRange = xRange.map((item, index) => `${item},${yRange[index]}`);
+  return xRange;
+}
+
+function problem2(array) {
+  const splitArray = array.map((item) =>
+    item
+      .split(" -> ")
+      .map((coordinate) =>
+        coordinate.split(",").map((item) => parseInt(item, 10))
+      )
+  );
+  const hOrV = splitArray.filter(
+    (item) => item[0][0] === item[1][0] || item[0][1] === item[1][1]
+  );
+  const diagonal = splitArray.filter((x) => !hOrV.includes(x));
+  const dia2 = diagonal.map((item) => processDiagonal(item)).flat();
+  const straight = hOrV
+    .map((item) =>
+      range(changingCoord(item)).map((it) =>
+        constantIdex(item)
+          ? `${it},${constantCoord(item)}`
+          : `${constantCoord(item)},${it}`
+      )
+    )
+    .flat();
+
+  const all = straight
+    .concat(dia2)
+    .filter(
+      (a, index, array) =>
+        array.indexOf(a) === index && array.lastIndexOf(a) !== index
+    ).length;
+  return all;
+}
+
+console.log(problem2(fileToArray("day5/input.txt")));
