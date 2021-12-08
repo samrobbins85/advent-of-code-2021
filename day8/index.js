39c36828cd199ade14c2f7124c7e9f874b9c229c
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-08 11:51:28 +0000

    Remove duplicated code

--- day8/index.js
+++ day8/index.js
@@ -28,51 +28,37 @@ function part2(array) {
   return array
     .map((item) => {
       let charSet = item.split(" | ")[0].split(" ");
+      let numbers = {};
+      function removeValue(value, index) {
+        numbers[index] = Array.from(value);
+        charSet.splice(charSet.indexOf(value), 1);
+      }
       const one = charSet.filter((item) => item.length === 2)[0];
-      charSet.splice(charSet.indexOf(one), 1);
+      removeValue(one, 1);
       const four = charSet.filter((item) => item.length === 4)[0];
-      charSet.splice(charSet.indexOf(four), 1);
+      removeValue(four, 4);
       const seven = charSet.filter((item) => item.length === 3)[0];
-      charSet.splice(charSet.indexOf(seven), 1);
+      removeValue(seven, 7);
       const eight = charSet.filter((item) => item.length === 7)[0];
-      charSet.splice(charSet.indexOf(eight), 1);
-      let numbers = {
-        0: null,
-        1: Array.from(one),
-        2: null,
-        3: null,
-        4: Array.from(four),
-        5: null,
-        6: null,
-        7: Array.from(seven),
-        8: Array.from(eight),
-        9: null,
-      };
-      const a = numbers[7].filter((x) => !numbers[1].includes(x))[0];
+      removeValue(eight, 8);
       const three = charSet
         .filter((item) => item.length === 5)
         .filter((arr) => numbers[1].every((item) => arr.includes(item)))[0];
-      charSet.splice(charSet.indexOf(three), 1);
-      numbers[3] = Array.from(three);
+      removeValue(three, 3);
       const six = charSet
         .filter((item) => item.length === 6)
         .filter((arr) => !numbers[1].every((item) => arr.includes(item)))[0];
-      charSet.splice(charSet.indexOf(six), 1);
-      numbers[6] = Array.from(six);
+      removeValue(six, 6);
       const nine = charSet
         .filter((item) => item.length === 6)
         .filter((arr) => numbers[4].every((item) => arr.includes(item)))[0];
-      charSet.splice(charSet.indexOf(nine), 1);
-      numbers[9] = Array.from(nine);
+      removeValue(nine, 9);
       const zero = charSet.filter((item) => item.length === 6)[0];
-      numbers[0] = Array.from(zero);
-      charSet.splice(charSet.indexOf(zero), 1);
+      removeValue(zero, 0);
       const c = numbers[8].filter((x) => !numbers[6].includes(x))[0];
       const two = charSet.filter((item) => Array.from(item).includes(c))[0];
-      numbers[2] = Array.from(two);
-      charSet.splice(charSet.indexOf(two), 1);
+      removeValue(two, 2);
       numbers[5] = Array.from(charSet[0]);
-
       let output = item.split(" | ")[1].split(" ");
       return parseInt(output.map((item) => decode(item, numbers)).join(""), 10);
     })
