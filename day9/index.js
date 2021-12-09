e0c880a871551a50a9e592f55dcc15c9ca85b5e9
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-09 13:30:02 +0000

    Day9 Part2

--- day9/index.js
+++ day9/index.js
@@ -21,4 +21,67 @@ function part1(array) {
   return output.reduce((prev, curr) => prev + curr);
 }
 
+function part2(array) {
+  const input = array.map((item) =>
+    Array.from(item).map((char) => parseInt(char, 10))
+  );
+  let outArr = JSON.parse(JSON.stringify(input));
+  let outCoords = [];
+  input.forEach((row, y) =>
+    row.forEach((cell, x) => {
+      if (
+        cell < (input?.[y]?.[x - 1] ?? 10) &&
+        cell < (input?.[y]?.[x + 1] ?? 10) &&
+        cell < (input?.[y + 1]?.[x] ?? 10) &&
+        cell < (input?.[y - 1]?.[x] ?? 10)
+      ) {
+        outCoords.push([y, x]);
+        outArr[y][x] = cell + 1;
+      } else {
+        outArr[y][x] = 0;
+      }
+    })
+  );
+  function getAdditionalCoords(coord) {
+    let coords = [];
+    if (input?.[coord[0]]?.[coord[1] - 1] < 9) {
+      coords.push([coord[0], coord[1] - 1]);
+    }
+    if (input?.[coord[0]]?.[coord[1] + 1] < 9) {
+      coords.push([coord[0], coord[1] + 1]);
+    }
+    if (input?.[coord[0] + 1]?.[coord[1]] < 9) {
+      coords.push([coord[0] + 1, coord[1]]);
+    }
+    if (input?.[coord[0] - 1]?.[coord[1]] < 9) {
+      coords.push([coord[0] - 1, coord[1]]);
+    }
+    return coords;
+  }
+  const result = outCoords
+    .map((item) => {
+      let includedCells = input.map((row) => row.map(() => 0));
+      includedCells[item[0]][item[1]] = 1;
+      let newItems = [item];
+      while (newItems.length > 0) {
+        let tempNewItems = [];
+        newItems.forEach((newItem) => {
+          const addi = getAdditionalCoords(newItem);
+          addi.forEach((entry) => {
+            if (includedCells[entry[0]][entry[1]] === 0) {
+              tempNewItems.push(entry);
+            }
+            includedCells[entry[0]][entry[1]] = 1;
+          });
+        });
+        newItems = tempNewItems;
+      }
+
+      return includedCells.flat().reduce((prev, curr) => prev + curr);
+    })
+    .sort((first, second) => second - first);
+  return result[0] * result[1] * result[2];
+}
+
 console.log(part1(fileToArray("day9/input.txt")));
+console.log(part2(fileToArray("day9/input.txt")));
