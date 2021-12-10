cbc4bf0cd0054da2d325c12f91f073cb699f1ec9
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-10 10:52:46 +0000

    Some tidying

--- day10/index.js
+++ day10/index.js
@@ -1,58 +1,54 @@
 import { fileToArray } from "../common/utils.js";
 
+const opening = ["(", "[", "{", "<"];
+const closing = [")", "]", "}", ">"];
+const penalties = {
+  ")": 3,
+  "]": 57,
+  "}": 1197,
+  ">": 25137,
+};
+const completion = {
+  "(": 1,
+  "[": 2,
+  "{": 3,
+  "<": 4,
+};
 function processEntry(array) {
-  const opening = ["(", "[", "{", "<"];
-  const closing = [")", "]", "}", ">"];
-  const penalties = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137,
-  };
   let stack = [];
   let score = 0;
   array.forEach((character) => {
     if (opening.includes(character)) {
       stack.push(character);
-    } else {
-      const top = stack.pop();
-      if (!(opening.indexOf(top) === closing.indexOf(character))) {
-        score += penalties[character];
-      }
+    } else if (!(opening.indexOf(stack.pop()) === closing.indexOf(character))) {
+      score += penalties[character];
     }
   });
   return { score, stack };
 }
 
 export function part1(array) {
-  const input = array.map((item) => Array.from(item));
-  const out = input.map((row) => {
-    const { score } = processEntry(row);
-    return score;
-  });
-  return out.reduce((prev, curr) => prev + curr);
+  return array
+    .map((item) => processEntry(Array.from(item)).score)
+    .reduce((prev, curr) => prev + curr);
 }
 
 export function part2(array) {
-  const input = array.map((item) => Array.from(item));
-  const completion = {
-    "(": 1,
-    "[": 2,
-    "{": 3,
-    "<": 4,
-  };
-  const out = input.map((row) => {
-    const { score, stack } = processEntry(row);
-    if (score === 0 && stack.length > 0) {
-      return stack
-        .map((item) => completion[item])
-        .reduceRight((prev, curr) => prev * 5 + curr, 0);
-    }
+  const out = array
+    .flatMap((row) => {
+      const { score, stack } = processEntry(Array.from(row));
+      if (score === 0 && stack.length > 0) {
+        return [
+          stack
+            .map((item) => completion[item])
+            .reduceRight((prev, curr) => prev * 5 + curr, 0),
+        ];
+      }
 
-    return null;
-  });
-  const sorted = out.filter(Boolean).sort((a, b) => a - b);
-  return sorted[Math.round(sorted.length / 2) - 1];
+      return [];
+    })
+    .sort((a, b) => a - b);
+  return out[Math.floor(out.length / 2)];
 }
 
 console.log(part1(fileToArray("day10/input.txt")));
