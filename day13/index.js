c6f2d6daeeec52e7c97875ae804e44c1c01f476b
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-13 16:12:53 +0000

    Simplify fold logic

--- day13/index.js
+++ day13/index.js
@@ -3,23 +3,16 @@ import { fileToArray } from "../common/utils.js";
 function processFolds(folds, array) {
     folds.forEach((fold) => {
         const foldPosition = parseInt(fold[1]);
-        array = array.map((item) =>
-            item.split(",").map((item) => parseInt(item, 10))
-        );
-        if (fold[0] === "y") {
-            array = array.map((item) =>
-                item[1] > foldPosition
-                    ? [item[0], foldPosition - (item[1] - foldPosition)]
-                    : item
-            );
-        } else {
-            array = array.map((item) =>
-                item[0] > foldPosition
-                    ? [foldPosition - (item[0] - foldPosition), item[1]]
-                    : item
-            );
-        }
         array = array
+            .map((item) => item.split(",").map((item) => parseInt(item, 10)))
+            .map((item) => {
+                if (fold[0] === "y" && item[1] > foldPosition) {
+                    return [item[0], foldPosition - (item[1] - foldPosition)];
+                } else if (fold[0] === "x" && item[0] > foldPosition) {
+                    return [foldPosition - (item[0] - foldPosition), item[1]];
+                }
+                return item;
+            })
             .map((item) => `${item[0]},${item[1]}`)
             .filter((item, index, arr) => arr.indexOf(item) === index);
     });
@@ -40,10 +33,10 @@ export function part2(array) {
         .splice(array.findIndex((item) => item === ""))
         .filter(Boolean)
         .map((item) => item.split("fold along ")[1].split("="));
-    array = processFolds(folds, array);
-    array = array.map((item) =>
+    array = processFolds(folds, array).map((item) =>
         item.split(",").map((item) => parseInt(item, 10))
     );
+
     const largestX = Math.max(...array.map((item) => item[0]));
     const largestY = Math.max(...array.map((item) => item[1]));
     let display = Array.from(Array(largestY + 1), () =>
