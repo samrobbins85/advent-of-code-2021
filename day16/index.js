36a31ca6eb5d10ceebf2396fa8b75488c80ffdb9
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-17 15:18:54 +0000

    Day16

--- day16/index.js
+++ day16/index.js
@@ -11,9 +11,11 @@ function typeIdFour(chunks) {
     const endOfString = chunks.findIndex((item) => item[0] === "0");
     // Nothing to do here yet
     const thisSection = chunks.slice(0, endOfString + 1);
+    console.log("Literal value:");
+    console.log(parseInt(thisSection, 2));
     const nextSection = chunks.slice(endOfString + 1);
     const trimmedRemainder = nextSection.join("");
-    return trimmedRemainder;
+    return { nextSection: trimmedRemainder, value: parseInt(thisSection, 2) };
 }
 
 export function part1(array) {
@@ -36,7 +38,7 @@ export function part1(array) {
             console.log("Found a Literal Packet");
             const remaining = input.substring(6);
             const chunks = remaining.match(/.{1,5}/g);
-            const nextSection = typeIdFour(chunks);
+            const { nextSection } = typeIdFour(chunks);
             processString(nextSection);
         } else {
             // Operator packet
@@ -61,4 +63,67 @@ export function part1(array) {
     console.log(processString(input));
     return score;
 }
-console.log(part1(fileToArray("day16/input.txt")));
+
+function part2Fresh(array) {
+    const input = Array.from(array[0])
+        .map((character) =>
+            Array.from(
+                parseInt(character, 16).toString(2).padStart(4, "0")
+            ).map((item) => parseInt(item, 10))
+        )
+        .flat();
+    let position = 0;
+    function parseArray(array) {
+        return parseInt(array.join(""), 2);
+    }
+    function nextNCharacters(length) {
+        const characters = input.slice(position, position + length);
+        position += length;
+        return characters;
+    }
+    function readPacket() {
+        position += 3;
+        const typeID = parseArray(nextNCharacters(3));
+        if (typeID === 4) {
+            const numbers = [];
+            while (true) {
+                const [prefix, ...remaining] = nextNCharacters(5);
+                numbers.push(...remaining);
+                if (prefix === 0) break;
+            }
+            return parseArray(numbers);
+        } else {
+            const lengthTypeID = parseArray(nextNCharacters(1));
+            let subValues = [];
+            let morePackets;
+            if (lengthTypeID === 0) {
+                const subLengthMax = parseArray(nextNCharacters(15));
+                const startPointer = position;
+                morePackets = () => position - startPointer < subLengthMax;
+            } else {
+                const subCountMax = parseArray(nextNCharacters(11));
+                morePackets = () => subValues.length < subCountMax;
+            }
+            while (morePackets()) subValues.push(readPacket());
+            switch (typeID) {
+                case 0:
+                    return subValues.reduce((p, c) => p + c, 0);
+                case 1:
+                    return subValues.reduce((p, c) => p * c, 1);
+                case 2:
+                    return Math.min(...subValues);
+                case 3:
+                    return Math.max(...subValues);
+                case 5:
+                    return subValues[0] > subValues[1] ? 1 : 0;
+                case 6:
+                    return subValues[0] < subValues[1] ? 1 : 0;
+                case 7:
+                    return subValues[0] === subValues[1] ? 1 : 0;
+            }
+        }
+    }
+    return readPacket();
+}
+
+console.log(part2Fresh(fileToArray("day16/input.txt")));
